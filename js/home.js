793bc48027d9a862a02638805529fa31b852d565
yasmin shapochkin <281905036+yasminshapochkin@users.noreply.github.com>
2026-07-02 14:20:03 +0300

    btn start working

---
+++ js/home.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded' , function(){
+    let btn_strt = document.getElementById('strtBTN');
+
+    btn_strt.addEventListener('click',function(){
+        window.location.href='login/login.html';
+
+    });
+    
+
+
+});
