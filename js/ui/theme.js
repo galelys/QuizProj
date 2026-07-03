a7310d224665f6fcf60c67a5cc0704ff1cb91266
yasmin shapochkin <281905036+yasminshapochkin@users.noreply.github.com>
2026-07-03 16:12:52 +0300

    dark mode updates

--- js/ui/theme.js
+++ js/ui/theme.js
@@ -1,22 +1,34 @@
-/**this file is the dark mode file! */
+// file to dark mode!
 
 export function initThemeToggle() {
     const saved = localStorage.getItem("theme");
+    const btn = document.getElementById("themeToggle");
 
     if (saved === "dark") {
         document.body.classList.add("dark-mode");
     }
 
-    const btn = document.getElementById("themeToggle");
+    function updateButtonText(btn) {
+        btn.textContent =
+            document.body.classList.contains("dark-mode")
+                ? "Light Mode"
+                : "Dark Mode";
+    }
 
     if (btn) {
+        updateButtonText(btn);
+
         btn.addEventListener("click", () => {
             document.body.classList.toggle("dark-mode");
 
             localStorage.setItem(
                 "theme",
-                document.body.classList.contains("dark-mode") ? "dark" : "light"
+                document.body.classList.contains("dark-mode")
+                    ? "dark"
+                    : "light"
             );
+
+            updateButtonText(btn);
         });
     }
 }
