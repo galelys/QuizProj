785e80f787513554a1bdf6fd14fdeb4405515f5a
yasmin shapochkin <281905036+yasminshapochkin@users.noreply.github.com>
2026-07-04 17:03:18 +0300

    create exam start

--- app/teacher/home.js
+++ app/teacher/home.js
@@ -3,21 +3,34 @@ import { UserService } from "../../js/services/UserService.js";
 import { initThemeToggle } from "../../js/ui/theme.js";
 
 document.addEventListener('DOMContentLoaded' , function(){
-
+    // dark mode button initialization 
     initThemeToggle();
+    // create button 
+    let create_bth = document.getElementById('createBTN');
+
+
     
+    // pulling the information aout this user 
     const urlParams = new URLSearchParams(window.location.search);
     const id = urlParams.get("id");
+    // updating the console that its working 
     console.log(id);
-
+    // verifing 
     const userService = new UserService();
     const user = userService.findUserById(id);
 
     console.log(user);
-    
+    // writing welcome <user name> on the page
     document.getElementById('title').textContent += user.name;
 
+    create_bth.addEventListener('click' , GoToCreate);
 
+    function GoToCreate(){
+        window.location.href = "./create/createExam.html";
+        //document.getElementById("contentFrame").src = "./create/createExam.html";
+    }
+
+    
 });
 
 
