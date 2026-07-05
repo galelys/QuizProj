e033fc8d350fb800026312a428ea96b4c685a870
yasmin shapochkin <281905036+yasminshapochkin@users.noreply.github.com>
2026-07-05 11:56:25 +0300

    Run page inisialized

--- app/ExamRunner/ExamRunner.js
+++ app/ExamRunner/ExamRunner.js
@@ -1,15 +1,13 @@
-import { Question } from "../../js/models/Question.js";
+import { Question } from "../../../js/models/Question.js";
 import { Exam } from "../../../../js/models/exam.js";
-import { ExamService } from "../../js/services/ExamService.js";
-import { ExamUI } from "../../js/ui/ExamUI.js";
-import { initThemeToggle } from "../../js/ui/theme.js";
+import { ExamService } from "../../../js/services/ExamService.js";
+import { ExamUI } from "../../../js/ui/ExamUI.js";
+import { initThemeToggle } from "../../../js/ui/theme.js";
 
-document.addEventListener('DOMContentLoaded' , function(){
-
+document.addEventListener('DOMContentLoaded', function () {
     initThemeToggle();
+
+    const examService = new ExamService();
+    const examUI = new ExamUI(examService);
     
-
-
-
-
 });
