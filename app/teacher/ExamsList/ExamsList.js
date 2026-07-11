e8a29e4ce10cfe2f61b124c847b1e929dc539427
Gal Elisha <jal425377@gmail.com>
2026-07-11 15:09:04 +0300

    fixed ID prob

--- app/teacher/ExamsList/ExamsList.js
+++ app/teacher/ExamsList/ExamsList.js
@@ -120,7 +120,7 @@ document.addEventListener('DOMContentLoaded', function () {
             // Remove exam from storage
             examService.deleteExam(examID);
             //remove the exam for all users
-            userService.removeExamResultsFromAllUsers(examId);
+            userService.removeExamResultsFromAllUsers(examID);
             // Refresh displayed list
             examUI.renderExamList("teacher");
         }
